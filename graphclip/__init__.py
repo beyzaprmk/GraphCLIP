1a8411739e640d3d5e6ec6ffc10920e2dec26809
Beyza Parmak <154760716+beyzaprmk@users.noreply.github.com>
2026-08-11 12:23:09 +0300

    feat: graphclip/__init__.py defines GraphCLIP's public API for external use.

---
+++ graphclip/__init__.py
@@ -0,0 +1,10 @@
+from inference.inference import GraphCLIPInference
+from inference.loader import load_model
+from model.graph_clip import GraphCLIP
+
+
+__all__ = [
+    "GraphCLIP",
+    "GraphCLIPInference",
+    "load_model",
+]
