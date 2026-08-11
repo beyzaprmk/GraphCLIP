198d1b77625badb3d87543031f679c0034225189
Beyza Parmak <154760716+beyzaprmk@users.noreply.github.com>
2026-08-11 12:06:22 +0300

    feat(loader): export ArtifactLoader and load_model from package root

--- inference/__init__.py
+++ inference/__init__.py
@@ -1,9 +1,16 @@
 from .factory import GraphCLIPFactory
-from .loader import CheckpointLoader
+from .loader import (
+    CheckpointLoader,
+    ArtifactLoader,
+    load_model,
+)
 from .predictor import GraphCLIPPredictor
+
 
 __all__ = [
     "GraphCLIPFactory",
     "CheckpointLoader",
+    "ArtifactLoader",
     "GraphCLIPPredictor",
+    "load_model",
 ]
